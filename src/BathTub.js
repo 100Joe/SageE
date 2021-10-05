a5ccbeaa18ffd3ed942cd4fc89fc7f1eadcdad0c
Joseph Edmonds <consultingjde@gmail.com>
2021-10-05 16:09:09 -0700

    working bathtub with water divs

--- src/BathTub.js
+++ src/BathTub.js
@@ -15,11 +15,14 @@ function BathTub() {
     }
     let intervalId;
     intervalId = setInterval(() => {
-      setWaterLevel(newWaterLevel => newWaterLevel + 20)
-    }, 1000)
+      setWaterLevel(newWaterLevel => newWaterLevel + 20);
+      const waterDiv = document.createElement("div");
+      waterDiv.className = "water";
+      document.querySelector(".tub").appendChild(waterDiv);
+    }, 2000)
     setTimeout(() => {
       clearInterval(intervalId)
-    }, 5000)
+    }, 10000)
   }, [increment])
 
   useEffect(() => {
@@ -30,22 +33,22 @@ function BathTub() {
     let intervalId;
     intervalId = setInterval(() => {
       setWaterLevel(newWaterLevel => newWaterLevel - 20)
-    }, 1000)
+      const myDiv = document.querySelector(".water");
+      document.querySelector(".tub").removeChild(myDiv);
+    }, 2000)
     setTimeout(() => {
       clearInterval(intervalId)
-    }, 5000)
+    }, 10000)
   }, [decrement])
 
   return (
     <div className="container">
-      <div className="tub" >
-        <div className="levelOn"></div>
-      </div>
+      <div className="tub"></div>
       <div className="level">
-        {waterLevel}%
+        {waterLevel}px
       </div>
-      <button onClick={() => setIncrement(!increment)} disabled={waterLevel !== 100 ? false : true}>IncreaseWaterLevel</button>
-      <button onClick={() => setDecrement(!decrement)} disabled={!waterLevel} >DecreaseWaterLevel</button>
+      <button className='btn-increase' onClick={() => setIncrement(!increment)} disabled={waterLevel !== 100 ? false : true}>IncreaseWaterLevel</button>
+      <button className='btn-decrease' onClick={() => setDecrement(!decrement)} disabled={!waterLevel} >DecreaseWaterLevel</button>
     </div>
   )
 }
