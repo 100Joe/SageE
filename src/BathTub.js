e09c2730341b7533e81c4f6c38b623cd24741c6a
Joseph Edmonds <consultingjde@gmail.com>
2021-10-05 13:15:35 -0700

    useEffect and waterLevel logic added

--- src/BathTub.js
+++ src/BathTub.js
@@ -1,42 +1,51 @@
-import { useState, } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './BathTub.css';
 
 function BathTub() {
   const [waterLevel, setWaterLevel] = useState(0)
+  const [increment, setIncrement] = useState(false)
+  const [decrement, setDecrement] = useState(false)
+  const firstUpdate = useRef(false)
+  const secondUpdate = useRef(true)
 
-  function increaseWaterLevel() {
-    setWaterLevel(newWaterLevel => newWaterLevel + 20)
-  }
+  useEffect(() => {
+    if (!firstUpdate.current) {
+      firstUpdate.current = true
+      return
+    }
+    let intervalId;
+    intervalId = setInterval(() => {
+      setWaterLevel(newWaterLevel => newWaterLevel + 20)
+    }, 1000)
+    setTimeout(() => {
+      clearInterval(intervalId)
+    }, 5000)
+  }, [increment])
 
-  // const waterDiv = Document.querySelector('levelOne');
+  useEffect(() => {
+    if (secondUpdate.current) {
+      secondUpdate.current = false
+      return
+    }
+    let intervalId;
+    intervalId = setInterval(() => {
+      setWaterLevel(newWaterLevel => newWaterLevel - 20)
+    }, 1000)
+    setTimeout(() => {
+      clearInterval(intervalId)
+    }, 5000)
+  }, [decrement])
 
-  function decreaseWaterLevel() {
-    setWaterLevel(newWaterLevel => newWaterLevel - 20)
-  }
-
-  const fill = () => setInterval(increaseWaterLevel, 1000)
-  const drain = () => setInterval(decreaseWaterLevel, 1000)
-  const water = () => setInterval(addWaterLevel, 2000)
-  const addWaterLevel = () => {
-    return <div className="levelOne"></div>;
-  }
   return (
     <div className="container">
       <div className="tub" >
-        {
-          fill ? addWaterLevel() : null
-        }
-        {/* <div className="levelOne"></div>
-          <div className="levelOne"></div>
-          <div className="levelOne"></div>
-          <div className="levelOne"></div>
-          <div className="levelOne"></div> */}
+        <div className="levelOn"></div>
       </div>
       <div className="level">
         {waterLevel}%
       </div>
-      <button onClick={() => { fill(); water(); }}>IncreaseWaterLevel</button>
-      <button onClick={drain}>DecreaseWaterLevel</button>
+      <button onClick={() => setIncrement(!increment)} disabled={waterLevel !== 100 ? false : true}>IncreaseWaterLevel</button>
+      <button onClick={() => setDecrement(!decrement)} disabled={!waterLevel} >DecreaseWaterLevel</button>
     </div>
   )
 }
